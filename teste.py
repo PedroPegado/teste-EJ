c05dcd337e0a605e5ab617b2749e994775497e37
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-04-08 01:19:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,16 @@
 from playwright.sync_api import sync_playwright
+import requests
+
 
 with sync_playwright() as p:
-    nav = p.chromium.launch(headless=False)
-    page = nav.new_page
-    page.goto('http://google.com.br')
-
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.uol.com.br/")
+    page.click('#l1pj0alt')    
+    link = page.url
+    page.wait_for_timeout(3000)
+    print(page.title())    
+    #browser.close()
+   
+response = requests.get(link)
+print(response.json())
